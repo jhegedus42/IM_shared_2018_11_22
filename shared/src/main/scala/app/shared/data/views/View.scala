22042e4931eeab9a4cf982952cc86c9efed9143d
Jozsef Hegedus <jhegedus42@gmail.com>
2018-04-25 22:21:10 +0300

    we went back, on the way, again, to implement View-s

--- shared/src/main/scala/app/shared/data/views/View.scala
+++ shared/src/main/scala/app/shared/data/views/View.scala
@@ -1,8 +1,34 @@
 package app.shared.data.views
 
+import app.shared.data.views.UserLineListView2.ViewPayLoad
+
 /**
   * Created by joco on 08/01/2018.
   */
 trait View {
+  type PayLoad <:ViewPayLoadType
+}
+
+trait ViewPayLoadType {
 
 }
+
+object UserLineListView2 extends View {
+  case class ViewPayLoad(kamuPayload:String) extends ViewPayLoadType {
+  }
+
+  // we need a type class that takes a ViewParameters and returns a ViewPayload - the business logic
+}
+
+object TestStuff extends App{
+ println("bla")
+  val pl1 = UserLineListView2.ViewPayLoad("bla")
+}
+
+object ConversionOnServerSide{
+
+  //  payload to json
+  //  json to parameters
+
+}
+
