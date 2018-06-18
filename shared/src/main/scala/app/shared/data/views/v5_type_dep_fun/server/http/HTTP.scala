0385cea2e217251d32325271c68da575e816292f
Jozsef Hegedus <jhegedus42@gmail.com>
2018-06-18 08:38:54 +0300

    whole server side model  works : creating Endpoint
    -defining View1's and their paramters
    type class for server logic that calculates the view
    endpoint that take the requests, trascodes it , forwards it to the logic, and takes the result , encodes it to json , send the result over the wire
    
    Remaining part:
    
    Modelling the Client side + caching on the client side.

--- shared/src/main/scala/app/shared/data/views/v5_type_dep_fun/server/http/HTTP.scala
+++ shared/src/main/scala/app/shared/data/views/v5_type_dep_fun/server/http/HTTP.scala
@@ -1,18 +1,22 @@
 package app.shared.data.views.v5_type_dep_fun.server.http
 
-import app.shared.data.views.v5_type_dep_fun.
-        server.logic.ServerSideLogic.ServerLogicTypeClass
-
+import app.shared.data.views.v5_type_dep_fun.server.logic.ServerSideLogic.ServerLogicTypeClass
 import app.shared.data.views.v5_type_dep_fun.shared.Shared.{View, View1}
 import io.circe.{Encoder, Json}
 import io.circe.parser._
 import io.circe.syntax._
-
-import app.shared.data.views.
-        v4_path_dep_type_based_view_architecture.shared.CirceUtils._
+import app.shared.data.views.v4_path_dep_type_based_view_architecture.shared.CirceUtils._
+import app.shared.data.views.v5_type_dep_fun.shared.Shared.View1.{View1, View1_Par}
 import io.circe.{Decoder, Error}
 
 
+import io.circe.Decoder
+
+import io.circe.{Decoder, Error}
+import io.circe.generic.auto._
+
+import io.circe._
+import io.circe.parser._
 
 case class JSON(s: String )
 
@@ -29,7 +33,7 @@ object EndpointCreator extends App {
       implicit
       decoder: Decoder[V#Par],
       encoder: Encoder[V#Res],
-      serverLogic:       ServerLogicTypeClass[V]
+      serverLogic:ServerLogicTypeClass[V]
     ): String = {
 
     val r:   Either[Error, V#Par] = decodeJSONToRes[V]( json.s )
@@ -41,7 +45,12 @@ object EndpointCreator extends App {
     encodeResToJSON[V](res)
   }
 
-  val par = View1.View1_Par("pina")
+  val par: View1_Par = View1.View1_Par("pina")
 
+  val par_json: String = encodeParToJSON[View1](par)
+
+  val r: String = f[View1](JSON(par_json))
+
+  println(r)
 
 }
