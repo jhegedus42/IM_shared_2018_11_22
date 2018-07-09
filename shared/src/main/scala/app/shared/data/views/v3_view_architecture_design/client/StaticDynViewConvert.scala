37b77773ba559734cf8e7dee9aa13ea038d47f01
Jozsef Hegedus <jhegedus42@gmail.com>
2018-07-09 18:44:38 +0300

    cleaning up

--- shared/src/main/scala/app/shared/data/views/v3_view_architecture_design/client/StaticDynViewConvert.scala
+++
@@ -1,8 +0,0 @@
-package app.shared.data.views.v3_view_architecture_design.client
-
-/**
-  * Created by joco on 02/06/2018.
-  */
-object StaticDynViewConvert{ //on client side
-
-}
--- shared/src/main/scala/app/shared/data/views/v3_view_architecture_design/shared/views/common/View.scala
+++
@@ -1,6 +0,0 @@
-package app.shared.data.views.v3_view_architecture_design.shared.views.common
-
-/**
-  * Created by joco on 02/06/2018.
-  */
-trait View
