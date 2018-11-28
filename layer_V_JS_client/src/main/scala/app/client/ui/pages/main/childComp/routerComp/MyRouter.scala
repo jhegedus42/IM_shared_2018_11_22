2b3406f0ef591e8f1ce56594c0f841a590180ae8
Jozsef Hegedus <jhegedus42@gmail.com>
2018-11-28 04:31:57 +0200

    kis tipusbabralas es lefordul

--- layer_V_JS_client/src/main/scala/app/client/ui/pages/main/childComp/routerComp/MyRouter.scala
+++ layer_V_JS_client/src/main/scala/app/client/ui/pages/main/childComp/routerComp/MyRouter.scala
@@ -2,7 +2,7 @@ package app.client.ui.pages.main.childComp.routerComp
 
 import app.client.entityCache.entityCacheV1.D2toD1Transformer
 import app.client.ui.pages.main.childComp.routerComp.childOfRouter.navigator.childOfNavigator.possibleChildrenOfNavigator_depth1Components.lineDetail.LineDetail_CompConstr_Holder
-import app.client.ui.pages.main.childComp.routerComp.childOfRouter.navigator.childOfNavigator.possibleChildrenOfNavigator_depth1Components.lineDetail.LineDetail_CompConstr_Holder.F.Res
+import app.client.ui.pages.main.childComp.routerComp.childOfRouter.navigator.childOfNavigator.possibleChildrenOfNavigator_depth1Components.lineDetail.LineDetail_CompConstr_Holder.F
 import app.client.ui.pages.main.childComp.routerComp.childOfRouter.navigator.childOfNavigator.possibleChildrenOfNavigator_depth1Components.lineList.LineListWrapping
 import app.client.ui.pages.main.childComp.routerComp.childOfRouter.navigator.childOfNavigator.possibleChildrenOfNavigator_depth1Components.listOfLineLists.UserLineListsCompConstr_Holder
 import app.client.ui.pages.main.childComp.routerComp.childOfRouter.navigator.childOfNavigator.{LineDetail_URL, LineList_Page, LineListsOfUser_URL, URL_STr}
@@ -28,10 +28,16 @@ object MyRouter {
       dsl: RouterConfigDsl[URL_STr] =>
         import dsl._
 
-        val f: D2toD1Transformer => (LineDetail_URL, RouterCtl[URL_STr]) => Res =
-           LineDetail_CompConstr_Holder.getCompConstr_For_dynRenderR_In_Router _
+//        val f: D2toD1Transformer => (LineDetail_URL, RouterCtl[URL_STr]) => Res =
+        val f: D2toD1Transformer => (LineDetail_URL, RouterCtl[URL_STr]) => F.Res =
+        {
 
-        val h: (LineDetail_URL, RouterCtl[URL_STr]) => Res = f(trD2toD1)
+          val r: D2toD1Transformer => F.Res2 = LineDetail_CompConstr_Holder.getCompConstr_For_dynRenderR_In_Router
+          ???
+        }
+
+        val h: (LineDetail_URL, RouterCtl[URL_STr]) => F.Res = f(trD2toD1)
+//        val h: F.Par  => F.Res = f(trD2toD1)
 
         val dr_lineDetail: dsl.Rule =
           dsl.dynamicRouteCT( "#item" / dsl.uuid.caseClass[LineDetail_URL] ) ~> dsl.dynRenderR(h)
