f2c6136c9770fa01aff3959a516eb183e174ab4b
Jozsef Hegedus <jhegedus42@gmail.com>
2018-12-04 01:06:00 +0200

    static simple component is set as root ... seems to compile ... will it run ???

--- layer_V_JS_client/src/main/scala/app/client/Main.scala
+++ layer_V_JS_client/src/main/scala/app/client/Main.scala
@@ -1,7 +1,6 @@
 package app.client
 
-import app.client.ui_v2.D0_MaterialUIWrapperComp
-import app.client.ui_v2.D0_MaterialUIWrapperComp.D0_RootCompConstructor
+import app.client.ui_v2.app.client.ui.pages.main.childComp.routerComp.childOfRouter.navigator.childOfNavigator.possibleChildrenOfNavigator_depth1Components.lineDetail.D0_SimpleComp
 import japgolly.scalajs.react.ReactDOM
 import org.scalajs.dom
 
@@ -19,14 +18,19 @@ object Main extends js.JSApp {
     LoggerConfig.level   = LogLevel.TRACE
 
 
-    val compConstructor : D0_RootCompConstructor = D0_MaterialUIWrapperComp.rootCompConstructor
+//    val compConstructor : D0_RootCompConstructor = D0_MaterialUIWrapperComp.rootCompConstructor
+    val compConstructor  = D0_SimpleComp.simpleCompConstr("hello42")
     // TODO 0 ^^^  replace this with a simple static react page that displays something very
-    // simple, like a text ... a hello world
+    // simple, like a text ... a hello world ... and then show that in the browser
+
+
 
 
 
 //    ReactDOM.render( compConstructor(), dom.document.getElementById( "joco" ) )
-    ReactDOM.render( compConstructor(), dom.document.getElementById( "joco" ) )
+    ReactDOM.render( compConstructor, dom.document.getElementById( "joco" ) )
   }
 }
+
+
 
---
+++ layer_V_JS_client/src/main/scala/app/client/ui_v2/D0_SimpleComp.scala
@@ -0,0 +1,38 @@
+package app.client.ui_v2
+
+package app.client.ui.pages.main.childComp.routerComp.childOfRouter.navigator.childOfNavigator.possibleChildrenOfNavigator_depth1Components.lineDetail
+
+import japgolly.scalajs.react.ReactComponentC.ReqProps
+import japgolly.scalajs.react.vdom.prefix_<^._
+import japgolly.scalajs.react.{BackendScope, ReactComponentB, ReactElement, TopNode}
+
+
+object D0_SimpleComp {
+
+  type CompConst = ReqProps[String, Unit, _, TopNode]
+
+  class BackendSimpleComp(backendScope: BackendScope[String, Unit] ) {
+
+    def render(depth2CompProps: String): ReactElement = {
+
+      <.div(
+             "props that the render got: ",
+             <.br,
+              depth2CompProps,
+             <.br,
+             "props that the render got printed with pretty print: ",
+             <.br,
+             <.br
+           )
+    }
+  }
+
+  val simpleCompConstr: CompConst = {
+    ReactComponentB[String]( "LineDetail" )
+    .backend[BackendSimpleComp](new BackendSimpleComp(_))
+    .renderBackend
+    .build
+  }
+  //
+
+}
