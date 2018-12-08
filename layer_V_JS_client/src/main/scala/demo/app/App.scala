6687e675cddbe8dd11dd8a786339b12431808241
Jozsef Hegedus <jhegedus42@gmail.com>
2018-12-09 01:20:52 +0200

    css jol muxik, normalisan nez ki a hello world oldal

--- layer_V_JS_client/src/main/scala/demo/app/App.scala
+++ layer_V_JS_client/src/main/scala/demo/app/App.scala
@@ -1,20 +1,20 @@
 package demo.app
 
 import demo.app.components.Main
+import org.scalajs.dom.document
+import org.scalajs.dom.raw.Element
+import scalajsreact.template.css.AppCSS
+import scalajsreact.template.routes.AppRouter
 
 import scala.scalajs.js.JSApp
-import org.scalajs.dom.document
-import CssSettings._
-import org.scalajs.dom.raw.Element
-import scalacss.ScalaCssReact._
-import scalajsreact.template.routes.AppRouter
 
 object App extends JSApp {
   def main(): Unit = {
     println("elkezd futni a demo App")
-    Styles.addToDocument()
+//    Styles.addToDocument()
     val e: Element = document.getElementById("rootComp")
     val e2: Element = document.getElementById("rootComp2")
+    AppCSS.load
     println("element="+e)
     println("element="+e2)
     Main().renderIntoDOM(e)
