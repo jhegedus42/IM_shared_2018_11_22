02f0aea623626996cd0c18f4900afc53369ca96f
Jozsef Hegedus <jhegedus42@gmail.com>
2018-12-30 15:28:55 +0200

    Iszonyatosan nagy kavaras van folyamatban :
    parameterezett Map-ban taroljuk az Entity-ket.... es

--- layer_V_JS_client/src/main/scala/experiments/cacheExperiments/cache/Cache.scala
+++ layer_V_JS_client/src/main/scala/experiments/cacheExperiments/cache/Cache.scala
@@ -1,9 +1,11 @@
 package experiments.cacheExperiments.cache
 
 import app.shared.SomeError_Trait
+import app.shared.data.model.Entity.Entity
 import app.shared.data.model.LineText
 import app.shared.data.ref.{Ref, RefVal}
 import app.testHelpersShared.data.TestEntities
+import experiments.cacheExperiments.cache.CacheStates.{CacheState, Loading}
 import scalaz.\/
 
 import scala.concurrent.{ExecutionContextExecutor, Future}
@@ -16,47 +18,58 @@ import scala.concurrent.{ExecutionContextExecutor, Future}
 //  }
 //}
 
-case class ReRenderTriggerer(triggerer: Unit => Unit )
+case class ReRenderTriggerer(triggerReRender: Unit => Unit )
 
-object AJAXApi {
+object AJAXApi {}
 
-  def fetchDataFromServer() = {
-    implicit def executionContext: ExecutionContextExecutor =
-      scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
-    import app.client.rest.commands.generalCRUD.GetEntityAJAX.getEntity
-    import io.circe.generic.auto._
-    val ref: Ref[LineText] = Ref.makeWithUUID[LineText]( TestEntities.refValOfLineV0.r.uuid )
-    println("elkuldunk a kovetkezo sorban egy ajax-ot")
-    val res: Future[Unit] = getEntity[LineText]( ref ).map(
-      x => {
-        println( s"az entity visszavage az AJAXApi-ban $x" )
-        val lt:  \/[SomeError_Trait, RefVal[LineText]] = x
-        val res: Option[RefVal[LineText]]              = lt.toOption
+object CacheStates {
+  sealed trait CacheState[E]
+  case class Loading[E](r: Ref[E] ) extends CacheState[E]
+  case class Loaded[E](r:  Ref[E], refVal: RefVal[E] ) extends CacheState[E]
+}
 
-        println( "we will update the cache here" )
-        Cache.lineTextOption = res // MURDEEEEERRRR BLOOOODDD HEELLLLLLL DAAAAANNNNNGGGGEEEEERRRR :)
-        println( "we will call here the re render trigger" )
-        val r = Cache.reRenderTriggerer.get // WE GONNA BURN IN HELLL FOR THIS UNSAFE GET !!!!
-        r.triggerer()
-        // here we trigger a re-render
-      }
-    )
+class EntityCacheMap[E <: Entity]() {
+  var map: Map[Ref[E], CacheState[E]] = Map()
+
+  def readEntity(refToEntity: Ref[E] ): CacheState[E] = {
+    if (!map.contains( refToEntity )) {
+      val loading = Loading( refToEntity )
+      pullEntityFromServerIntoCache()
+    } else map.get( refToEntity )
+
+
+
+    def pullEntityFromServerIntoCache(ref: Ref[E] ) = {
+      implicit def executionContext: ExecutionContextExecutor =
+        scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
+      import app.client.rest.commands.generalCRUD.GetEntityAJAX.getEntity
+      import io.circe.generic.auto._
+      val ref: Ref[LineText] = Ref.makeWithUUID[LineText]( TestEntities.refValOfLineV0.r.uuid )
+      println( "elkuldunk a kovetkezo sorban egy ajax-ot" )
+      val res: Future[Unit] = getEntity[LineText]( ref ).map(
+        x => {
+          println( s"az entity visszavage az AJAXApi-ban $x" )
+          val lt:  \/[SomeError_Trait, RefVal[LineText]] = x
+          val res: Option[RefVal[LineText]]              = lt.toOption
+
+          println( "we will update the cache here" )
+          Cache.map = res // MURDEEEEERRRR BLOOOODDD HEELLLLLLL DAAAAANNNNNGGGGEEEEERRRR :)
+          println( "we will call here the re render trigger" )
+          val r = Cache.reRenderTriggerer.get // WE GONNA BURN IN HELLL FOR THIS UNSAFE GET !!!!
+          r.triggerReRender()
+          // here we trigger a re-render
+        }
+      )
+    }
+
   }
+
 }
 
 object Cache {
 
   //cache will have a separate map for each entity, each view
 
-  type State = Option[RefVal[LineText]]
-  var lineTextOption: State = None
-
   var reRenderTriggerer: Option[ReRenderTriggerer] = None
 
-  def read(): State = {
-    if (lineTextOption.isEmpty) {
-      AJAXApi.fetchDataFromServer()
-      None
-    } else lineTextOption
-  }
 }
